ad40ef492f0b8c14a3f1ee42bd54cb936f926a77
Steve Billings <sbillings@blackducksoftware.com>
2020-01-09 20:23:43 -0500

    test: PolarisBuildStepTest continued development

--- src/test/java/com/synopsys/integration/jenkins/polaris/extensions/buildstep/PolarisBuildStepTest.java
+++ src/test/java/com/synopsys/integration/jenkins/polaris/extensions/buildstep/PolarisBuildStepTest.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Optional;
 
 import org.junit.Ignore;
 import org.junit.Rule;
@@ -15,6 +16,8 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.synopsys.integration.jenkins.polaris.extensions.tools.PolarisCli;
 
 import hudson.EnvVars;
 import hudson.FilePath;
@@ -30,16 +33,15 @@ import hudson.remoting.VirtualChannel;
 
 @PowerMockIgnore({"javax.crypto.*", "javax.net.ssl.*" })
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({AbstractBuild.class, JDK.class, Launcher.class, Launcher.ProcStarter.class})
+@PrepareForTest({AbstractBuild.class, JDK.class, Launcher.class, Launcher.ProcStarter.class, PolarisCli.class})
 public class PolarisBuildStepTest {
     private static final String POLARIS_ARGUMENTS = "--detect.docker.passthrough.service.timeout=240000 --detect.cleanup=false --detect.source.path=$JAVA_HOME --detect.project.name=\"Test Project'\"";
-    private static final String WORKSPACE_REL_PATH = "out/test/DetectPostBuildStepTest/testPerform/workspace";
+    private static final String WORKSPACE_REL_PATH = "out/test/PolarisBuildStepTest/testPerform/workspace";
     private static final String javaHomePath = System.getenv("JAVA_HOME");
 
     @Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
 
-    @Ignore
     @Test
     public void test() throws IOException, InterruptedException {
         final AbstractBuild<FreeStyleProject, FreeStyleBuild> build = PowerMockito.mock(AbstractBuild.class);
@@ -67,6 +69,15 @@ public class PolarisBuildStepTest {
 
         final VirtualChannel channel = PowerMockito.mock(hudson.remoting.VirtualChannel.class);
         Mockito.when(launcher.getChannel()).thenReturn(channel);
+
+        PowerMockito.mockStatic(PolarisCli.class);
+        final PolarisCli polarisCli = PowerMockito.mock(PolarisCli.class);
+        Mockito.when(PolarisCli.findInstanceWithName("testPolarisCliName")).thenReturn(Optional.of(polarisCli));
+
+        // polarisCli.forEnvironment(envVars);
+        Mockito.when(polarisCli.forEnvironment(Mockito.any(EnvVars.class))).thenReturn(polarisCli);
+        // polarisCli.forNode(node, listener);
+        Mockito.when(polarisCli.forNode(Mockito.any(Node.class), Mockito.any(TaskListener.class))).thenReturn(polarisCli);
 
         final PolarisBuildStep polarisBuildStep = new PolarisBuildStep("testPolarisCliName", POLARIS_ARGUMENTS);
         polarisBuildStep.perform(build, launcher, buildListener);
