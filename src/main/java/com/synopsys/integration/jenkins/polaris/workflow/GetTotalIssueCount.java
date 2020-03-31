b4b25aad8619c2fb710179920c9b5fa2b6e1c40f
Richard Otte <rotte@blackducksoftware.com>
2020-03-31 17:41:32 -0400

    feat: Support both 1.X and 2.X versions of the cli-scan.json

--- src/main/java/com/synopsys/integration/jenkins/polaris/workflow/GetTotalIssueCount.java
+++ src/main/java/com/synopsys/integration/jenkins/polaris/workflow/GetTotalIssueCount.java
@@ -22,8 +22,6 @@
  */
 package com.synopsys.integration.jenkins.polaris.workflow;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -31,16 +29,14 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.synopsys.integration.exception.IntegrationException;
 import com.synopsys.integration.jenkins.extensions.JenkinsIntLogger;
-import com.synopsys.integration.polaris.common.PolarisCliResponseVersion;
 import com.synopsys.integration.polaris.common.api.query.model.CountV0;
 import com.synopsys.integration.polaris.common.api.query.model.CountV0Attributes;
 import com.synopsys.integration.polaris.common.api.query.model.CountV0Resources;
 import com.synopsys.integration.polaris.common.cli.PolarisCliResponseUtility;
-import com.synopsys.integration.polaris.common.cli.model.BlackDuckScaToolInfo;
-import com.synopsys.integration.polaris.common.cli.model.CoverityToolInfo;
-import com.synopsys.integration.polaris.common.cli.model.IssueSummary;
-import com.synopsys.integration.polaris.common.cli.model.PolarisCliResponseModel;
-import com.synopsys.integration.polaris.common.cli.model.ScanInfo;
+import com.synopsys.integration.polaris.common.cli.model.CliCommonResponseModel;
+import com.synopsys.integration.polaris.common.cli.model.CommonIssueSummary;
+import com.synopsys.integration.polaris.common.cli.model.CommonScanInfo;
+import com.synopsys.integration.polaris.common.cli.model.CommonToolInfo;
 import com.synopsys.integration.polaris.common.exception.PolarisIntegrationException;
 import com.synopsys.integration.polaris.common.request.PolarisRequestFactory;
 import com.synopsys.integration.polaris.common.service.JobService;
@@ -49,7 +45,6 @@ import com.synopsys.integration.stepworkflow.SubStep;
 import com.synopsys.integration.stepworkflow.SubStepResponse;
 
 public class GetTotalIssueCount implements SubStep<String, Integer> {
-    public static final Integer SUPPORTED_POLARIS_CLI_RESPONSE_MAJOR_VERSION = 1;
     public static final String STEP_EXCEPTION_PREFIX = "Issue count for most recent Polaris Analysis could not be determined: ";
     private final JenkinsIntLogger logger;
     private final PolarisService polarisService;
@@ -71,51 +66,36 @@ public class GetTotalIssueCount implements SubStep<String, Integer> {
 
         final PolarisCliResponseUtility polarisCliResponseUtility = PolarisCliResponseUtility.defaultUtility(logger);
         final String rawJson = previousResponse.getData();
-        final PolarisCliResponseModel polarisCliResponseModel = polarisCliResponseUtility.getPolarisCliResponseModelFromString(rawJson);
-
         try {
-            final String versionString = polarisCliResponseModel.getVersion();
-            final PolarisCliResponseVersion version = PolarisCliResponseVersion.parse(versionString)
-                                                          .orElseThrow(() -> new PolarisIntegrationException(STEP_EXCEPTION_PREFIX + "Version string " + versionString + " is not a valid version. "
-                                                                                                                 + "Please verify that you are using a supported version of the Polaris CLI."));
-            if (!SUPPORTED_POLARIS_CLI_RESPONSE_MAJOR_VERSION.equals(version.getMajor())) {
-                throw new PolarisIntegrationException(
-                    STEP_EXCEPTION_PREFIX + "The version of the cli-scan.json is " + versionString + " and this version of Synopsys Polaris for Jenkins only supports major version " + SUPPORTED_POLARIS_CLI_RESPONSE_MAJOR_VERSION
-                        + ". Please verify that you are using a supported version of the Polaris CLI.");
-            }
+            final CliCommonResponseModel polarisCliResponseModel = polarisCliResponseUtility.getPolarisCliResponseModelFromString(rawJson);
 
-            final IssueSummary issueSummary = polarisCliResponseModel.getIssueSummary();
-            final ScanInfo scanInfo = polarisCliResponseModel.getScanInfo();
+            final Optional<CommonIssueSummary> issueSummary = polarisCliResponseModel.getIssueSummary();
+            final CommonScanInfo scanInfo = polarisCliResponseModel.getScanInfo();
 
-            if (issueSummary != null) {
+            if (issueSummary.isPresent()) {
                 logger.debug("Found total issue count in cli-scan.json, scan must have been run with -w");
-                return SubStepResponse.SUCCESS(issueSummary.getTotalIssueCount());
+                return SubStepResponse.SUCCESS(issueSummary.get().getTotalIssueCount());
             }
 
             if (jobTimeoutInMinutes < 0) {
-                throw new PolarisIntegrationException("Issue count for most recent Polaris Analysis could not be determined: job timeout must be a positive number if the Polaris CLI is being run without -w");
+                throw new PolarisIntegrationException(STEP_EXCEPTION_PREFIX + "Job timeout must be a positive number if the Polaris CLI is being run without -w");
             }
 
             final String issueApiUrl = Optional.ofNullable(scanInfo)
-                                           .map(ScanInfo::getIssueApiUrl)
+                                           .map(CommonScanInfo::getIssueApiUrl)
                                            .filter(StringUtils::isNotBlank)
                                            .orElseThrow(() -> new PolarisIntegrationException(
                                                "Synopsys Polaris for Jenkins cannot find the total issue count or issue api url in the cli-scan.json. Please ensure that you are using a supported version of the Polaris CLI."
                                            ));
 
             logger.debug("Found issue api url, polling for job status");
-            final List<String> jobStatusUrls = new ArrayList<>();
 
-            Optional.ofNullable(polarisCliResponseModel.getBlackDuckScaToolInfo())
-                .map(BlackDuckScaToolInfo::getJobStatusUrl)
-                .ifPresent(jobStatusUrls::add);
-
-            Optional.ofNullable(polarisCliResponseModel.getCoverityToolInfo())
-                .map(CoverityToolInfo::getJobStatusUrl)
-                .ifPresent(jobStatusUrls::add);
-
-            for (final String jobStatusUrl : jobStatusUrls) {
-                jobService.waitForJobStateIsCompletedOrDieByUrl(jobStatusUrl, jobTimeoutInMinutes, JobService.DEFAULT_WAIT_INTERVAL_IN_SECONDS);
+            for (final CommonToolInfo tool : polarisCliResponseModel.getTools()) {
+                final String jobStatusUrl = tool.getJobStatusUrl();
+                if (jobStatusUrl == null) {
+                    throw new PolarisIntegrationException(STEP_EXCEPTION_PREFIX + "tool with name " + tool.getToolName() + " has no jobStatusUrl");
+                }
+                jobService.waitForJobStateIsCompletedOrDieByUrl(jobStatusUrl);
             }
 
             final CountV0Resources countV0Resources = polarisService.get(CountV0Resources.class, PolarisRequestFactory.createDefaultBuilder().uri(issueApiUrl).build());
